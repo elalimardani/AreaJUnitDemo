4778f75fa927319067684746e7368b3869c13b1d
elalimardani <58517842+elalimardani@users.noreply.github.com>
2020-09-18 08:44:10 -0400

    added:circle

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
 
-        System.out.println("Let's get your points, how many points would you like to define? At this time, answer can be a number between 3 and 5.\n");
+        System.out.println("Let's get your points, how many points would you like to define? At this time, answer can be a number between 3 and 0.\n");
         Scanner scanner = new Scanner(System.in);
 
         int pointNum = scanner.nextInt();
@@ -20,12 +20,15 @@ public class Main {
                 System.out.println("The area of your defined triangle is: " + Area.triangleArea(a,b,c));
                 break;
 
-            case 4:
-                System.out.println("Your shape is a rectangle");
+            case 0:
+                System.out.println("Your shape is a circle. \n Please enter the radius of your circle:");
+                int radius = scanner.nextInt();
+                System.out.println("The area of your defined circle is: " + Area.circleAreaWithMathPI(radius));
                 break;
+
+            default:
+                System.out.println("We cannot deal with your shape at this moment! Check back later");
         }
-
-
 
     }
 }
